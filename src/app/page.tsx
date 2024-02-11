16f673645693b876832c71ecdbd54bb02347211e
eziiel <eziielp10@hotmail.com>
2024-02-11 15:57:45 -0300

    CHORE:form skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FormAccess from '@/components/form-access'
+
 export default function Home() {
   return (
-    <main>
-      <header>financial control</header>
-      <section>form access</section>
-      <footer>footer</footer>
+    <main className="w-full h-screen bg-red-300 flex justify-center">
+      {/* <header>financial control</header> */}
+      <FormAccess />
+      {/* <footer>footer</footer> */}
     </main>
   )
 }
