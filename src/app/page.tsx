f45d86d61067a1e588a6b6a11cb2ced6eeb1b0e7
eziiel <eziielp10@hotmail.com>
2024-02-11 17:48:02 -0300

    STYLE:form-access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import FormAccess from '@/components/form-access'
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-red-300 flex justify-center">
+    <main
+      className="w-full min-h-screen bg-white flex justify-center
+    "
+    >
       {/* <header>financial control</header> */}
       <FormAccess />
       {/* <footer>footer</footer> */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        blackOne: '0 0 50px 10px rgba(0, 0, 0, 0.5)',
+        bigComponentBlack: '0 0 30px 10px rgba(0, 0, 0, 0.3)',
+        bigComponentBlue: '0 0 30px 0.5px  rgba(0, 0, 255, 0.3)',
+        bigComponentRed: '0 0 30px 0.5px  rgba(255, 0, 0, 0.3)',
+        bigComponentGreen: '0 0 30px 0.5px   rgba(0, 255, 0, 0.3)',
+        inset: 'inset 0 0 100px 10px  rgba(0, 0, 0, 0.3)',
+      },
     },
   },
   plugins: [],
